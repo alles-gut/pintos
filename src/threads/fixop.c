b7e851ce8ed6f758ea2a2f2ceac2349cfef5dbe7
alles-gut <jhu4142@gmail.com>
2020-05-02 09:24:28 +0900

    fixop modify

--- src/threads/fixop.c
+++ src/threads/fixop.c
@@ -1,17 +1,3 @@
-#include <stdint.h>
-#define FRA (1<<14)
-
-/*
-int int_mis_flt (int, int);
-int int_mul_flt (int, int);
-int flt_pls_int (int, int);
-int flt_div_int (int, int);
-int flt_pls_flt (int, int);
-int flt_mis_flt (int, int);
-int flt_mul_flt (int, int);
-int flt_div_flt (int, int);
-*/
-
 int int_mis_flt (int i, int f)
 {
   return i*FRA - f;
--- src/threads/fixop.h
+++ src/threads/fixop.h
@@ -13,49 +13,4 @@ int flt_mis_flt (int, int);
 int flt_mul_flt (int, int);
 int flt_div_flt (int, int);
 
-/*
-int int_mis_flt (int i, int f)
-{
-  return i*FRA - f;
-}
-
-int int_mul_flt (int i, int f)
-{
-  return i*f;
-}
-
-int flt_pls_int (int f, int i)
-{
-  return f + i*FRA;
-}
-
-int flt_div_int (int f, int i)
-{
-  return f / i;
-}
-
-int flt_pls_flt (int f, int f_)
-{
-  return f + f_;
-}
-
-int flt_mis_flt (int f, int f_)
-{
-  return f - f_;
-}
-
-int flt_mul_flt (int f, int f_)
-{
-  int64_t temp = f;
-  temp = temp * f_ / FRA;
-  return (int)temp;
-}
-
-int flt_div_flt (int f, int f_)
-{
-  int64_t temp = f;
-  temp = temp * FRA / f_;
-  return (int)temp;
-}
-*/
 #endif
